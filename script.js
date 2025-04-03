f61813da0fb692449f2be4e7473734c3caecf53e
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2025-04-03 14:06:49 -0600

    Calculadora de pollero

---
+++ script.js
@@ -0,0 +1,19 @@
+const pantalla = document.querySelector(".grid-pantalla");
+
+
+const button = document.querySelector("button");
+
+button.onclick = function BotoDeNumeros (){
+    let num1 = "miBoton"
+    document.getElementById("miBoton");
+    console.log("miBoton");
+    alert("miBoton");
+ };
+
+ 
+
+
+
+
+
+
