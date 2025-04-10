31e45fe927adcd6e07e2d2a4447ead603535250f
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2025-04-10 12:41:34 -0600

    Create jess.js

---
+++ Jess/jess.js
@@ -0,0 +1,11 @@
+
+console.log("¡Hola, mundo!");
+// Definir las variables de base y altura
+var base = 10;
+var altura = 5;
+
+// Calcular el área del triángulo
+var area = (base * altura) / 2;
+
+// Mostrar el resultado en la consola
+console.log("El área del triángulo es: " + area);
