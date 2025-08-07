3bbbbc96948629343ba3fa1daad2a3a919e0ec35
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2025-08-06 20:02:33 -0600

    Create objetos.js
    Objetos en JS

---
+++ CursoJS/objetos.js
@@ -0,0 +1,40 @@
+// Objeto math
+
+let res;
+
+res= Math.PI;
+res= Math.round(2.5);
+res= Math.ceil(2.1);    //redondeo hacia arriba
+res= Math.floor(2.9);   //redondero hacia abajo
+res= Math.sqrt(144);
+res= Math.abs(-400);    //absoluto
+res= Math.min () 
+
+console.log(res);
+
+// operaciones 
+
+let resultado;
+let resultado1;
+let impuestos;
+
+resultado = 20 + 30 *2;
+resultado1 = (20 + 30)*2;
+impuestos = (600 + 300 + 900) * 1.16;
+
+console.log(resultado);
+console.log(resultado1);
+console.log(impuestos);
+
+//incrementos
+
+puntaje = 10;
+
+console.log(puntaje++); // muestra valor -> luego incremento
+console.log(++puntaje); // incremento -> muestra valor
+
+puntaje += 100; // incremento en 100
+console.log(puntaje);
+
+puntaje -= 200; // decrementa en 200
+console.log(puntaje);
