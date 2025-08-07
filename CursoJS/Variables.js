5718d0459572b4a0bd462a30e57a799958194444
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2025-08-06 19:55:05 -0600

    Create Variables.js
    Variables en JS

---
+++ CursoJS/Variables.js
@@ -0,0 +1,16 @@
+//variables var=let const
+
+var producto = 'Audifonos gamer'
+let precio = '50'
+const fijo = '16' //valor fijo
+
+var disponible; //inicia variavle sin valor
+
+// producto = true; // reasignacion de valor
+var a = 'computadora' //multiples variables
+    b = 'true'
+    c = 'teclado'; 
+
+console.log(producto);
+console.log(precio);
+console.log(fijo);
