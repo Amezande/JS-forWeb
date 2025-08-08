bee194501575a0a089c01bea8aab53be8ca2a311
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2025-08-07 19:04:04 -0600

    Update 5-. ObjModif.js

--- CursoJS/5-. ObjModif.js
+++ CursoJS/5-. ObjModif.js
@@ -15,3 +15,20 @@ Object.seal(producto); //previene agregar, borrar y eliminar , si modifica
 console.log(Object.isFrozen(producto)); //verifica si el objeto esta bloqueado
 console.log(Object.isSealed(producto)); //verifica si el objeto esta bloqueado
 producto.imagen = 'imagen.jpg' ;
+
+// Union de clases de objetos
+const producto = {
+    nombreProducto : "Monitor 20 pulgadas",
+    precio : 300,
+    disponible : true
+}
+
+const medidas = {
+    peso: '1kg',
+    medida: '1m'
+}
+
+const nuevoProducto = { ...producto, ...medidas };
+
+console.log(producto);
+console.log(nuevoProducto); 
